415d298b45fefbdc9270feeb4771a5ebc253dffa
Chris Johnson <chrisjohnson00@hotmail.com>
2023-03-27 16:25:40 -0700

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pulsar-consumer
 A sample Pulsar consumer in Python
+
+## PyPi Dependency updates
+
+    docker run -it --rm -v ${PWD}:/repo -w /repo python:3.10.7-slim bash
+    pip install --upgrade pip
+    pip install --upgrade pulsar-client pygogo
+    pip freeze > requirements.txt
---
+++ app.py
@@ -0,0 +1,62 @@
+import os
+from json import loads, dumps
+import os.path
+import pygogo as gogo
+import pulsar
+import random
+import time
+
+# logging setup
+kwargs = {}
+formatter = gogo.formatters.structured_formatter
+logger = gogo.Gogo('struct', low_formatter=formatter).get_logger(**kwargs)
+
+
+def main():
+    logger.info("Starting Consumer!!")
+
+    client = pulsar.Client(f"pulsar://{get_config('PULSAR_SERVER')}")
+    consumer = client.subscribe(get_config('PULSAR_TOPIC'), get_config('PULSAR_SUBSCRIPTION'),
+                                consumer_type=pulsar.ConsumerType.Shared)
+
+    while True:
+        msg = consumer.receive()
+        try:
+            # decode from bytes, encode with backslashes removed, decode back to a string, then load it as a dict
+            message_body = loads(msg.data().decode().encode('latin1', 'backslashreplace').decode('unicode-escape'))
+            logger.debug("Message body", extra={'message_body': message_body})
+            consumer.acknowledge(msg)
+        except:  # noqa: E722
+            # Message failed to be processed
+            consumer.negative_acknowledge(msg)
+
+    client.close()
+
+
+def main_producer():
+    logger.info("Starting Producer")
+    topic = get_config("PULSAR_TOPIC")
+    pulsar_server = get_config('PULSAR_SERVER')
+    client = pulsar.Client(f"pulsar://{pulsar_server}")
+    producer = client.create_producer(topic)
+    duration = 3600
+    for i in range(duration):
+        message = {'number': random.randint(0, 10000)}
+        producer.send(dumps(message).encode('utf-8'))
+        logger.info("Notification sent",
+                    extra={'message_body': message, 'topic': topic})
+        time.sleep(1)
+    client.close()
+
+
+def get_config(key):
+    if os.environ.get(key):
+        logger.info("found {} in an environment variable".format(key))
+        return os.environ.get(key)
+
+
+if __name__ == '__main__':
+    if os.environ.get("PRODUCER_MODE"):
+        main_producer()
+    else:
+        main()
